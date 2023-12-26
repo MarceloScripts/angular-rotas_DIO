51902f603304d30441249095c132345531a4107f
MarceloScripts <marcelomjr77.7@gmail.com>
2023-12-26 14:28:26 -0300

    update project

---
+++ src/app/pages/portfolio/card/card.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  //http://localhost:4200/portfolio/{1}
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador: Router
+    ) {
+    this.activeRoute.params.subscribe(
+      res => console.log(res) 
+    )
+    this.activeRoute.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+  //http://localhost:4200/portfolio/1?{name=Marcelo&token=123}
+    this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+
+  }
+
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.navegador.navigate(['/'])
+    // }, 5000)
+  }
+
+}
